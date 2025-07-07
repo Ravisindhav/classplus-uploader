fc2d17be685af2ee3f67501a44c501ad2f433d91
Ravisindhav <sindhavravi11@gmail.com>
2025-07-07 18:32:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "29211134"
-api_hash = "bd48e9f3e02e98c3dc44b9ddf10d71ff"
-bot_token = "7691099356:AAEyetNAwplPsUQk6nUy-gd1-QNb_ZdUIoI"
-auth_users = [1416110833]
-sudo_users = [1416110833]
+api_id = ""
+api_hash = ""
+bot_token = ""
+auth_users = ""
+sudo_users = ""
